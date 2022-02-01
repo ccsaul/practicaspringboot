19c6b172402e36717f629f38ba8ab88e8eeb83a6
Saul Cocoletzi <saul.cocoletzi@hotmail.com>
2022-02-01 00:39:15 -0600

    se agregan carpetas

--- fundamnetos/src/main/java/com/findamentos/springboot/fundamentos/FundamnetosApplication.java
+++ fundamnetos/src/main/java/com/findamentos/springboot/fundamentos/FundamnetosApplication.java
@@ -1,4 +1,4 @@
-package com.findamentos.springboot.fundamnetos;
+package com.findamentos.springboot.fundamentos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
