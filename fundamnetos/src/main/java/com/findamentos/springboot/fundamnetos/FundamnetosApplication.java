b894d9850573a06980635a1fd4c7fdfe13c57b98
Saul Cocoletzi <saul.cocoletzi@hotmail.com>
2022-02-01 00:23:43 -0600

    subir codigo

---
+++ fundamnetos/src/main/java/com/findamentos/springboot/fundamnetos/FundamnetosApplication.java
@@ -0,0 +1,13 @@
+package com.findamentos.springboot.fundamnetos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamnetosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamnetosApplication.class, args);
+	}
+
+}
---
+++ fundamnetos/src/test/java/com/findamentos/springboot/fundamnetos/FundamnetosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.findamentos.springboot.fundamnetos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamnetosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
